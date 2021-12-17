46b02e842c9a92f610d0a19a1f4e8760f4d50104
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-12-17 14:27:11 +0100

    Returning content from our server

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const http = require("http"); // require http module - a core node module
+const fs = require("fs");
 
 const server = http.createServer((request, response) => {
   console.log("Request made");
   console.log(request.url, request.method);
-}); // use .createServer method to create a server
+
+  // set header content type
+  response.setHeader("Content-Type", "text/html");
+
+  fs.readFile("./pages/index.html", (error, data) => {
+    if (error) {
+      console.log(error);
+      response.end();
+    } else {
+      response.write(data);
+      response.end();
+    }
+  });
+}); // use .createServer method to create a server with request and response objects as the arguments
 
 server.listen(3000, "localhost", () => {
   console.log("listening for request on port 3000");
