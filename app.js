89c62b83f47cd28023217720131c9864c49ae3c7
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-12-19 10:28:07 +0100

    feat: learnt about middlewares, installed third party middleware package 'morgan' and used the .static() express method to let static files be accessible to the browser

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 // express app
 const app = express();
@@ -9,6 +10,18 @@ app.set("views", "pages"); // specify the folder name. default is views
 
 // listen for requests
 app.listen(3000);
+
+app.use((req, res, next) => {
+  console.log("new request made");
+  console.log(`host: ${req.hostname}`);
+  console.log(`path: ${req.path}`);
+  console.log(`method: ${req.method}`);
+  next(); //used to tell the middleware to proceed to the next function after running
+}); //middleware that runs between the browser's request and the server's response - .use() method
+
+app.use(express.static("public")); //.static() method allows us to select static files that should be made accessible to the user from the browser.
+app.use(morgan("dev")); // morgan is a third party middleware that logs the request details to the console
+app.use(morgan("tiny")); // you can choose the format you want the details to be outputted
 
 //choose the request you want to listen for
 app.get("/", (req, res) => {
@@ -36,7 +49,7 @@ app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
 });
 
-//404 page
+//404 page. Middleware that acts like a catch all to output the 404 page.
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
