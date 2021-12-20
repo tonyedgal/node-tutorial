a9b24f609ea347556888438751e55b4d0b13cffe
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-12-20 10:59:25 +0100

    feat: using mongoose and mongo to send data to a database and viewing and working with the data through sandbox routes

--- app.js
+++ app.js
@@ -1,30 +1,59 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
+
+// connect to mongodb
 const dbURI =
-  "mongodb+srv://spaceman:node-tut-db@node-tut.8hflk.mongodb.net/node-tut?retryWrites=true&w=majority";
+  "mongodb+srv://spaceman:node-tut-db@node-tut.8hflk.mongodb.net/node-tuts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }) // stop the URL deprecation warning in the terminal
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 //register view engine
 app.set("view engine", "ejs");
 app.set("views", "pages"); // specify the folder name. default is views
 
-// listen for requests
-app.listen(3000);
-
-app.use((req, res, next) => {
-  console.log("new request made");
-  console.log(`host: ${req.hostname}`);
-  console.log(`path: ${req.path}`);
-  console.log(`method: ${req.method}`);
-  next(); //used to tell the middleware to proceed to the next function after running
-}); //middleware that runs between the browser's request and the server's response - .use() method
-
 app.use(express.static("public")); //.static() method allows us to select static files that should be made accessible to the user from the browser.
 app.use(morgan("dev")); // morgan is a third party middleware that logs the request details to the console
 app.use(morgan("tiny")); // you can choose the format you want the details to be outputted
+
+// mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog 2",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("61c04ecf83f96706cb2bd455")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
 
 //choose the request you want to listen for
 app.get("/", (req, res) => {
